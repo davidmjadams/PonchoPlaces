34d70c272bfb8e801563e5404b3bc7d0867a92d8
davidmjadams <davidmjadams@gmail.com>
2026-02-13 09:54:47 +0000

    get basic config working

--- scripts/_shared/config.mjs
+++ scripts/_shared/config.mjs
@@ -1,4 +1,4 @@
-export const DEFAULT_DATABASE_URL = 'postgresql://postgres:postgres@127.0.0.1:54322/postgres';
+export const DEFAULT_DATABASE_URL = 'postgresql://postgres:postgres@127.0.0.1:54352/postgres';
 
 export function getDatabaseUrl(env = process.env) {
 	return env.DATABASE_URL ?? DEFAULT_DATABASE_URL;
